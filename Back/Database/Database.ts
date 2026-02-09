3878cb4c957301a976013a12e1fb82389c6ecd2a
unknown <arnaud.defrancq@laposte.net>
2026-02-09 20:45:05 +0100

    [+] Database : Ajout de la class Databse (OK) + Modif dans le tsconfig pour accepter les undefined

---
+++ Back/Database/Database.ts
@@ -0,0 +1,46 @@
+import mysql from "mysql2/promise";
+import "dotenv/config";
+
+
+export class Database {
+    private static pool: mysql.Pool | null = null;
+
+    public static initPool(): void{
+        if (this.pool) {
+            console.warn("Database connection already");
+            return;
+        }
+        try {
+            console.log(process.env.DB_HOST)
+            this.pool = mysql.createPool({
+                host: process.env.DB_HOST,
+                port: Number(process.env.DB_PORT),
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_DATABASE,
+                connectionLimit: 10, // Nombre max de connexions simultanées
+            });
+            console.log('Database Pool initialized');
+        } catch (error) {
+            console.error('Initialization failed : ', error);
+            return;
+        }
+    }
+
+    public static getPool(): mysql.Pool {
+        if (!this.pool) {
+            throw new Error('connection not init');
+        }
+        return this.pool;
+    }
+
+    public static async closePool(): Promise<void> {
+        if (this.pool) {
+            await this.pool.end();
+            this.pool = null;
+            console.log('Database pool closed');
+        } else {
+            console.warn('No database connection to close')
+        }
+    }
+}
