bc922a0de7f67adc62b23cbf0a56470faed3c787
unknown <arnaud.defrancq@laposte.net>
2026-02-16 10:40:38 +0100

    [~] Schema : Modif du schema loansDetailsSearch avec jour du optional()

--- Back/src/Schemas/LoansDetailsSearchSchema.ts
+++ Back/src/Schemas/LoansDetailsSearchSchema.ts
@@ -2,14 +2,12 @@ import * as z from "zod";
 
 // Création du schema de validation
 export const LoansDetailsSearchSchema = z.object({
-    id_loan: z.number().positive(),
-    user_name: z.string().max(50),
-    user_firstname: z.string().max(50),
-    book_title: z.string().max(255),
-    author_name: z.string().max(50),
-    date_borrowed: z.number().positive().int(),
-    date_rendered: z.number().positive().int(),
-    status: z.string().max(15)
+    id_loan: z.number().positive().optional(),
+    user_name: z.string().max(50).optional(),
+    user_firstname: z.string().max(50).optional(),
+    book_title: z.string().max(255).optional(),
+    author_name: z.string().max(50).optional(),
+    status: z.string().max(15).optional()
 })
 
 
