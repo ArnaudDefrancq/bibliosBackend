6fead1522b21cc518148f17c285c650b46322506
unknown <arnaud.defrancq@laposte.net>
2026-02-16 10:48:01 +0100

    [~] Schema : Modif dans les params de recherche

--- Back/src/Schemas/LoansDetailsSearchSchema.ts
+++ Back/src/Schemas/LoansDetailsSearchSchema.ts
@@ -2,11 +2,9 @@ import * as z from "zod";
 
 // Création du schema de validation
 export const LoansDetailsSearchSchema = z.object({
-    id_loan: z.number().positive().optional(),
     user_name: z.string().max(50).optional(),
     user_firstname: z.string().max(50).optional(),
     book_title: z.string().max(255).optional(),
-    author_name: z.string().max(50).optional(),
     status: z.string().max(15).optional()
 })
 
