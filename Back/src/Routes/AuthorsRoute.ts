f42c5686624defbc0d9682356d57c6a6c1641888
ArnaudDefrancq <90774246+ArnaudDefrancq@users.noreply.github.com>
2026-02-17 13:24:02 +0100

    [+] Route : Mise en place des routes pour Author (Pas tester)

---
+++ Back/src/Routes/AuthorsRoute.ts
@@ -0,0 +1,19 @@
+import express, { Router } from "express";
+import { AuthorsController } from "../Controllers/AuthorsController.js";
+
+export class AuthorsRoute {
+    public router: Router;
+
+    constructor() {
+        this.router = express.Router();
+        this.initializeRoutes();
+    }
+
+    private initializeRoutes(): void {
+        this.router.post('/author', AuthorsController.createAuthor);
+        this.router.get('/', AuthorsController.findAuthors);
+        this.router.get('/:id', AuthorsController.findAuthorById);
+        this.router.put('/:id', AuthorsController.updateAuthor);
+        this.router.delete('/:id', AuthorsController.deleteAuthor)
+    }
+}
