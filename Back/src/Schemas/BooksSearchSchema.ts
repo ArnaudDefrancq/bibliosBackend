b0a3feb05ed3d662b174db31fc4f9781f86f373c
unknown <arnaud.defrancq@laposte.net>
2026-02-25 08:34:29 +0100

    [+] Schema : Ajour du id_author pour le schema de recherche

--- Back/src/Schemas/BooksSearchSchema.ts
+++ Back/src/Schemas/BooksSearchSchema.ts
@@ -2,6 +2,7 @@ import * as z from "zod";
 
 export const BooksSearchSchema = z.object({
     title: z.string().max(200).optional(),
+    id_author: z.number().positive().int().optional(),
     limit: z.coerce.number().int().positive().optional()
 })
 
