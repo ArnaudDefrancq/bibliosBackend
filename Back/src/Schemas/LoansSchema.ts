fc569ec909bf93567bde1c34e24fb5d59680459b
unknown <arnaud.defrancq@laposte.net>
2026-03-05 15:20:54 +0100

    [+] Schema : Ajout du schema pour update un emprunt

--- Back/src/Schemas/LoansSchema.ts
+++ Back/src/Schemas/LoansSchema.ts
@@ -1,13 +1,20 @@
 import * as z from "zod";
 
+
 // Création du schema de validation
 export const LoansSchema = z.object({
-    id_lian: z.number().positive().optional(),
+    id_loan: z.number().positive().optional(),
     id_book: z.number().positive().int(),
     id_user: z.number().positive().int(),
-    date_borrowed: z.number().positive().int(),
+    date_borrowed: z.number().positive().int().default(() => Math.floor(Date.now() / 1000)),
     date_rendered: z.number().positive().int().default(0)
 })
+
+export const LoanUpdateSchema = LoansSchema.partial().omit({ 
+    id_book: true, 
+    id_user: true ,
+    date_borrowed: true
+});
 
 // Création de l'interface
 export type Loans = z.infer<typeof LoansSchema>
